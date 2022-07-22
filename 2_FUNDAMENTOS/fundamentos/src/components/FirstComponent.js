e14dc9fde3f204cc258b3108f2e197d0fe60e90f
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-21 23:22:18 -0300

    criado comentarios

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,11 +1,20 @@
-const FirstComponent = ()=>{
+//Arquivos de estilo
 
-    return(
+const FirstComponent = () => {
+
+    //isso é um comentario da função
+
+    /*
+    Comentarios com multiplas linhas 
+    */
+
+    return (
         <div>
+            {/* Comentario dentroe do JSX */}
             <h1>Meu primeiro componente</h1>
         </div>
     )
-    
+
 }
 
 export default FirstComponent
