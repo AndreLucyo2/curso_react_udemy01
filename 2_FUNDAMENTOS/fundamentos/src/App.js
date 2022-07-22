a7757512f3fee2e0a310969dd2fb0ce9068c1bdf
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-21 23:30:04 -0300

    expresions

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -3,12 +3,14 @@ import FirstComponent from './components/FirstComponent';
 
 //styles /css
 import './App.css';
+import TemplateExpressions from './components/TemplateExpressions';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos</h1>   
-      <FirstComponent/> 
+      <FirstComponent/>
+      <TemplateExpressions/> 
     </div>
   );
 }
