ba6aa5e6fe5fd529f740c6a453fdb694d628c2e4
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-23 16:30:43 -0300

    previus state

--- 3_AVANCANDO_NO_REACT/avancando/src/components/ListRender.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/components/ListRender.js
@@ -4,14 +4,29 @@ const ListRender = () => {
 
     const [minhaLista] = useState(["Pedro", "Maria", "Thiago", "Lucas"])
 
-    const [lsUsers] = useState([
+    const [lsUsers, setUsers] = useState([
         { id: 1, name: "Andre", age: 31 },
-        { id: 15654, name: "Lucas", age: 48 },
-        { id: 48, name: "Maria", age: 27 },
-        { id: 7785, name: "Pedro", age: 8 },
-        { id: 234255, name: "Andreia", age: 39 },
-        { id: 8, name: "Dentinho", age: 92 },
+        { id: 2, name: "Lucas", age: 48 },
+        { id: 3, name: "Maria", age: 27 },
+        { id: 4, name: "Pedro", age: 8 },
+        { id: 5, name: "Andreia", age: 39 },
+        { id: 6, name: "Dentinho", age: 92 },
     ]);
+
+    //tem acesso ao estado antigo
+    const deleteRandom = () => {
+        //gera um numero aleatorio e arredonda para baixo
+        const randomNumber = Math.floor(Math.random() * 7);
+
+        setUsers((prevUsers) => {
+            //Lista no estado atual antes de remover
+            console.log(prevUsers);
+
+            //Recarrega lista filtrando o que tenha id diferente id gerado
+            return prevUsers.filter((user) => randomNumber !== user.id);
+        });
+
+    };
 
     return (
         <div>
@@ -27,6 +42,8 @@ const ListRender = () => {
                     <li key={user.id} >{user.name} - {user.age}</li>
                 ))}
             </ul>
+            <h3>Usando o previu astate para manipular a lista:</h3>
+            <button onClick={deleteRandom}>Deletar user aleatorio</button>
         </div>
     )
 }
