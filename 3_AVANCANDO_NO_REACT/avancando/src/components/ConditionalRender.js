2dcf43ae216497dde455c27bf455e67598e05961
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-23 16:53:53 -0300

    renderização com base em condições

--- 3_AVANCANDO_NO_REACT/avancando/src/components/ConditionalRender.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/components/ConditionalRender.js
@@ -4,13 +4,25 @@ const ConditionalRender = () => {
 
     const [condicao] = useState(false);
 
+    const [name, setName] = useState('Andre');
+
     return (
         <div>
             <h1>Isso será exibido?</h1>
-            {/* Condiciona ternaria */}
+            {/* Condiciona ternaria simples */}
             {condicao && <p>Se a condicao for true, sim</p>}
-            {/* Condiciona ternaria */}
+            {/* Condiciona ternaria simples */}
             {!condicao && <p>Agora a condicao é falso</p>}
+
+            {/* Condiciona ternaria */}
+            <h1>If ternário:</h1>
+            {name === "João"?(
+                <div>O nome é {name} </div>
+            ):(
+                <div>Nome não encontrado!</div>
+            )}
+            {/* Altera o estado e ja rerenderiza a tela */}
+            <button onClick={()=>setName("João")}>Altera nome</button>
         </div>
     )
 }
