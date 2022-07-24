9deac621f3fee6b6192913e70dbba8d37b87e966
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-24 17:33:58 -0300

    pegando o valor do input com o usState

--- 5_FORM_EM_REACT/forms/src/components/MyForm.js
+++ 5_FORM_EM_REACT/forms/src/components/MyForm.js
@@ -1,13 +1,31 @@
 import './MyForm.css'
+import { useState } from 'react';
 
 function MyForm() {
+    /* 3 - Gerenciar dados do input - obter o dado digitado */
+    const[name, setName] = useState();
+    const[email, setEmail] = useState();
+
+    //Lidar com o nome:
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+
+    //pega o stado alterado no momento peganod o valor do unput
+    console.log(name)
+
     return (
         <div>
             <form>
                 {/* 1 - Criando o form */}
                 <div>
                     <label htmlFor="name">Nome</label>
-                    <input type="text" name="name" placeholder="Digite o seu nome" />
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Digite o seu nome"
+                        onChange={handleName}
+                    />
                 </div>
 
                 {/* 2 - Label envolvendo o input - REcomendado! */}
