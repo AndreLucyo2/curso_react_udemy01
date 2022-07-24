f899b32c54f1399c606e9e642e63aea638fb05fb
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-24 14:02:36 -0300

    inline

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -4,13 +4,20 @@ import MyComponent from './components/MyComponent';
 function App() {
   return (
     <div className="App">
-      
+
       {/* CSS Global */}
       <h1>React com CSS</h1>
 
       {/* CSS de componente */}
-      <MyComponent/>      
+      <MyComponent />
       <p>Este é um paragrafo do App.js casa CSS</p>
+      {/* Inline CSS */}
+      <p style={{ color: "blueviolet", padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
     </div>
   );
 }
