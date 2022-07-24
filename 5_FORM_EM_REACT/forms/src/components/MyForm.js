80379e05210f6dee510e853d803c6034935339a4
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-24 17:16:52 -0300

    label envolvendo o input

--- 5_FORM_EM_REACT/forms/src/components/MyForm.js
+++ 5_FORM_EM_REACT/forms/src/components/MyForm.js
@@ -3,12 +3,18 @@ import './MyForm.css'
 function MyForm() {
     return (
         <div>
-            {/* 1 - Criando o form */}
             <form>
+                {/* 1 - Criando o form */}
                 <div>
                     <label htmlFor="name">Nome</label>
                     <input type="text" name="name" placeholder="Digite o seu nome" />
                 </div>
+
+                {/* 2 - Label envolvendo o input - REcomendado! */}
+                <label>
+                    <span>E-mail</span>
+                    <input type="email" name="email" placeholder="Digite o seu e-mail" />
+                </label>
                 <input type="button" value="Enviar" />
             </form>
         </div>
