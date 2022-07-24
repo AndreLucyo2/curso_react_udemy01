accd180380a444fcd1013c37edc22456839cdde1
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-24 13:55:10 -0300

    css por compoente

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      
+      {/* CSS Global */}
       <h1>React com CSS</h1>
+
+      {/* CSS de componente */}
+      <MyComponent/>      
+      <p>Este é um paragrafo do App.js casa CSS</p>
     </div>
   );
 }
---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './MyComponent.css'
+
+const MyComponent = () => {
+    return (
+      <div>
+        <h1>CSS de componente</h1>
+        <p>Este parágrafo do componente</p>
+        <p className="my-comp-paragraph">Este é o parágrafo do componente com CSS do componente </p>
+      </div>
+    );
+  };
+export default MyComponent
