e7fce2ace16c053cbee0fa0da2e7765c9e0e0491
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-25 21:27:08 -0300

    criando logica para o start creen

--- 6_PROJETO_SECRET_WORD/secretword/src/App.js
+++ 6_PROJETO_SECRET_WORD/secretword/src/App.js
@@ -1,13 +1,32 @@
+//Hooks react
+import { useCallback, useEffect, useState } from "react";
+
 // components
 import StartScreen from "./components/StartScreen";
 
 // styles
 import "./App.css";
 
+// data
+import { wordsList } from "./data/words";
+
+// Stagios do jogo: Inicio , jogo e game over
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 function App() {
+
+  //Defininco o stagio inicial:
+  const [gameStage, setGameStage] = useState(stages[0].name);
+
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <StartScreen />}
+      {gameStage === "end" && <StartScreen />}
     </div>
   );
 }
