b7bd2e980d85b1956133ec8ecaac93abaa234615
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-27 22:22:34 -0300

    add coments

--- 7_REQ_HTTP_REACT/httpreact/src/App.js
+++ 7_REQ_HTTP_REACT/httpreact/src/App.js
@@ -10,15 +10,19 @@ function App() {
 
   const [products, setProducts] = useState([]);
 
-  // 1 - Resgatando dados:
+  // 1 - GET - Resgatando dados:
   useEffect(() => {
-
+    //Faz um GET nos db.json
     async function fetchData() {
+      //faz o get e aguarda a response
       const resp = await fetch(ulrBaseAPI);
+      //pega a resonse a converte para objeto js
       const data = await resp.json();
+      //seta os dados
       setProducts(data);
     };
 
+    //chama a função:
     fetchData();
 
   }, []);
