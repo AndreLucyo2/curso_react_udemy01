514e6b52657fb8b8273edc0d1132345b7af80c27
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-27 22:07:51 -0300

    useEffect de forma errada no react

--- 7_REQ_HTTP_REACT/httpreact/src/App.js
+++ 7_REQ_HTTP_REACT/httpreact/src/App.js
@@ -1,7 +1,25 @@
 
 import './App.css';
 
+//react
+import { useState, useEffect } from 'react'
+
+const ulrBaseAPI = "http://localhost:3000/products"
+
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  // 1 - Resgatando dados:
+  useEffect(async () => {
+    const resp = await fetch(ulrBaseAPI);
+    const data = await resp.json();
+    setProducts(data);
+
+  },[]);
+
+  console.log(products);
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
