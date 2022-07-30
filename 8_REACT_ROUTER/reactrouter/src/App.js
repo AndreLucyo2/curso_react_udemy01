c2779de89bc624a26bf03a44767852854b62fa6e
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-30 12:11:55 -0300

    Rotas nao existentes - rota não encontrada

--- 8_REACT_ROUTER/reactrouter/src/App.js
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -12,6 +12,7 @@ import About from './pages/About'
 import Navbar from './components/Navbar';
 import Product from './pages/Product';
 import Info from './pages/Info';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -27,6 +28,8 @@ function App() {
           <Route path='/products/:id' element={<Product />} />
           {/* 6 - nested rout - rota mais complexas */}
           <Route path='/products/:id/info' element={<Info />} />
+          {/* 7 - Rotas nao existentes - rota não encontrada*/}
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
       <h1>Rodapé</h1>
