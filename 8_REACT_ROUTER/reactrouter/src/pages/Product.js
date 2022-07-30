7d3a14aeed7db0d76b79564021c0489ad614239c
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-30 11:26:28 -0300

    carregando um dado individualmente pela rota API

--- 8_REACT_ROUTER/reactrouter/src/pages/Product.js
+++ 8_REACT_ROUTER/reactrouter/src/pages/Product.js
@@ -1,17 +1,34 @@
 import React from 'react'
 
 import { useParams } from 'react-router-dom'
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
 
-    /* 4 - rota dinamica */
+    // 4 - rota dinamica
     //o parametro deve ser igual ao informado no Route, neste caso foi id -->> path='/produts/:id'
-    const {id} = useParams();
+    const { id } = useParams();
 
-    //recebe informação do parametro e pode ser utilizado aqui
+    // 5 - Carregamento de um dado individual com base no ID utilizando url dinamica para a API
+    const url = "http://localhost:3000/products/" + id;
+    
+    //useFetch hook
+    const { data: product, loading, error } = useFetch(url);
+    //Carregar o objeto:
+    console.log(product);
+
     return (
         <>
-        <p>ID do produto: {id}</p>
+            <p>ID do produto: {id}</p>
+            {loading && <p>Carregando dados...</p>}
+            {error && <p>{error}</p>}
+            {product && (
+                <div>
+                    <h1>{product.name}</h1>
+                    <p>R${product.price}</p>
+                </div>
+            )}
+
         </>
     )
 }
