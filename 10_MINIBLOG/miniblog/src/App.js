a67f9b9bb662e883b9c3eff50b37438d0871f02c
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-31 22:41:40 -0300

    CSS navbar e footer

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -3,21 +3,27 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 //Styles:
 import './App.css';
-import About from "./pages/About/About";
+
+//components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 //Pages:
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
           </Routes>
         </div>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
