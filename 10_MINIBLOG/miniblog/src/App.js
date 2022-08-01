0e296e3a9c6ff1869c1a305997acd4fbc2f00527
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-31 23:11:50 -0300

    criando form de cadastro e login - criado rotas

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -11,19 +11,23 @@ import Footer from "./components/Footer";
 //Pages:
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
           </Routes>
         </div>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
