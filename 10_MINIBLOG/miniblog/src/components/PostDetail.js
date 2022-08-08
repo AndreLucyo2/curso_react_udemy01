0352576ceb74c7856c93bdd5d7c1d4209c22ed6d
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-07 22:28:41 -0300

    CSS dos detalhes do post

--- 10_MINIBLOG/miniblog/src/components/PostDetail.js
+++ 10_MINIBLOG/miniblog/src/components/PostDetail.js
@@ -3,10 +3,17 @@ import { Link } from "react-router-dom";
 import styles from "./PostDetail.module.css";
 
 const PostDetail = ({ post }) => {
+
     return (
         <div className={styles.post_detail}>
+
+            {/* mostra a imagem */}
             <img src={post.image} alt={post.title} />
+
+            {/* mostra o titulo */}
             <h2>{post.title}</h2>
+
+            {/* mostra o autor */}
             <p className={styles.createdby}>por: {post.createdBy}</p>
 
             {/* mostra os dados do post */}
@@ -14,12 +21,14 @@ const PostDetail = ({ post }) => {
                 {/* Pega os campos de cada documento na colection do firebase */}
                 {post.tags.map((tag) => (
                     <p key={tag}>
+                        {/* Mostra as tags */}
                         <span>#</span>
                         {tag}
                     </p>
                 ))}
             </div>
-            {/* Para ler o post individual */}
+
+            {/* Para ler o post individual redireciona para posts/id... */}
             <Link to={`/posts/${post.id}`} className="btn btn-outline">
                 Ler
             </Link>
