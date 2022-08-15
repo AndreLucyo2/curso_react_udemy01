165659821d56c2511380a7c29268850463723bc2
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-14 21:24:28 -0300

    Executa só quando um valor específico é alterado

--- 11_REACT_HOOKS/reacthooks/src/components/HookUseEffect.js
+++ 11_REACT_HOOKS/reacthooks/src/components/HookUseEffect.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 
+//componente:
 const HookUseEffect = () => {
 
-    // 1 - useEffect, sem dependencias
+    //--- Executa sempre que o componente muda  ---------------------------
+    // 1 - useEffect, sem dependencias, executa sempre que o componente muda
     // Executa sempre ao renderizar componente, da um escopo definido
     useEffect(() => {
-        console.log("Estou sendo executado!");
+        console.log("Estou sendo executado, pois meu componente mudou!");
     });
 
     //contador para o exemplo
@@ -15,6 +17,7 @@ const HookUseEffect = () => {
         setNumber(number + 1);
     };
 
+    //--- Executa só quando o componente é carregado  ----------------------
     // 2 - array de dependências vazio, quando quer executar algo apenas uma vez
     useEffect(() => {
         //mesmo que alterar algo, ele nunca mais é executado
@@ -22,12 +25,28 @@ const HookUseEffect = () => {
     }, []);
 
 
+    //---- Executa só quando um valor específico é alterado  ---------------
+    // 3 - item no array de dependências
+    const [anotherNumber, setAnotherNumber] = useState(0);
+
+    useEffect(() => {
+
+        if (anotherNumber > 0) {
+            console.log("Sou executado apenas quando a dependencia anotherNumber muda!");
+        }
+        //passa uma dependencia que sera monitorado o valor   
+    }, [anotherNumber]);
 
     return (
         <div>
             <h2>useEffect</h2>
             <p>Number: {number}</p>
             <button onClick={changeSomething}>Executar!</button>
+            <br />
+            <p>Another Number: {anotherNumber}</p>
+            <button onClick={() => setAnotherNumber(anotherNumber + 1)}>
+                Mudar anotherNumber!
+            </button>
             <hr />
         </div>
     );
