e3261b3442e046e126ad7c3cc0adee789a02bbdd
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-15 22:19:11 -0300

    useRef e DOM

--- 11_REACT_HOOKS/reacthooks/src/components/HookUseRef.js
+++ 11_REACT_HOOKS/reacthooks/src/components/HookUseRef.js
@@ -14,6 +14,21 @@ const HookUseRef = () => {
     //setCounter(counter +1);
   });
 
+  //  2 - useRef e dom
+  //ele recebe o input como referencia, permitindo ter acesso a todas as propriedade do input e manipulalos
+  const inputRef = useRef();
+  //cria um referencia do input
+  const [text, setText] = useState("");
+  //cria o evento de submit, 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Limpa o conteudo 
+    setText("");
+
+    //após a ação ele retorna o foco no input através do ref
+    inputRef.current.focus();
+  };
 
   return (
     <div>
@@ -24,6 +39,20 @@ const HookUseRef = () => {
       <button onClick={() => setCounter(counter + 1)}>Contador A</button>
       <p>Counter 2: {counterB}</p>
       <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
+
+      {/* 2 - useRef e DOM */}
+      <h3>useRef e DOM</h3>
+      <form onSubmit={handleSubmit}>
+        {/* linka o input com a referencia criada */}
+        <input
+          type="text"
+          ref={inputRef}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+
       <hr />
     </div>
   )
