336f6267b0b3abc7e98f9f59c20b2a669023be17
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-15 23:19:40 -0300

    useDebugValue

--- 11_REACT_HOOKS/reacthooks/src/hooks/usePrevious.js
+++ 11_REACT_HOOKS/reacthooks/src/hooks/usePrevious.js
@@ -3,17 +3,18 @@ import { useEffect, useRef, useDebugValue } from "react";
 //hook de exemplo
 export const usePrevious = (value) => {
 
-  const ref = useRef;
+    //guarda o estado antido de um valor, pode mapear como estava antes de alterar
+    const ref = useRef;
 
-  //useDebugValue
-  useDebugValue("-- CUSTOM HOOK COM USEDEBUGVALUE --");
-  useDebugValue("O número anterior é:" + value);
+    //useDebugValue
+    useDebugValue("-- CUSTOM HOOK COM USEDEBUGVALUE --");
+    useDebugValue("O número anterior é:" + value);
 
-  //rota sempre que o componente for solicitado
-  //guarda o estado antido de um valor, pode mapear como estava antes de alterar
-  useEffect(() => {
-    ref.current = value;
-  });
+    //rota sempre que o componente for solicitado
+    //guarda o estado antido de um valor, pode mapear como estava antes de alterar
+    useEffect(() => {
+        ref.current = value;
+    });
 
-  return ref.current;
+    return ref.current;
 };
