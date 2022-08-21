68ba80b688682a260c88fef26a006f1b95ab7e4d
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-21 20:49:49 -0300

    Validações de criação de usuário

---
+++ 12_REACTGRAM/backend/middlewares/userValidations.js
@@ -0,0 +1,45 @@
+//---- VALIDA O CORPO DA REQUISIÇÃO DO BODY  --------------------------------------
+
+//recebe o body da requisição e permite validar cada propriedade:
+const { body } = require("express-validator");
+
+const userCreateValidation = () => {
+    //validações por campo do body
+    return [
+        body("name")
+            .isString()
+            .withMessage("O nome é obrigatório.")
+            .isLength({ min: 3 })
+            .withMessage("O nome precisa ter no mínimo 3 caracteres."),
+
+        body("email")
+            .isString()
+            .withMessage("O e-mail é obrigatório.")
+            .isEmail()
+            .withMessage("Insira um e-mail válido"),
+
+        body("password")
+            .isString()
+            .withMessage("A senha é obrigatória.")
+            .isLength({ min: 5 })
+            .withMessage("A senha precisa de no mínimo 5 caracteres."),
+
+        body("confirmPassword")
+            .isString()
+            .withMessage("A confirmação de senha é obrigatória.")
+            .custom((value, { req }) => {
+                //Validação customizada: 
+                if (value != req.body.password) {
+                    //gera uma erro:
+                    throw new Error("As senhas não são iguais.");
+                }
+                //Se der erro retonra true
+                return true;
+            }),
+    ];
+};
+
+//Exporta como objeto:
+module.exports = {
+    userCreateValidation,
+};
--- 12_REACTGRAM/backend/routes/UserRoutes.js
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -5,11 +5,14 @@ const router = express.Router();
 // Controller, recebe o objeto e desestrutura cada função
 const { register, } = require("../controllers/UserController");
 
-// Middlewares, validações da requisição
-const validate = require("../middlewares/handleValidations");
+// Middlewares, validações da requisição: resgata os erros gerados
+const validate = require("../middlewares/handleValidation");
+
+//importa as validações do usuario: ele captura os erros gerados 
+const { userCreateValidation } = require("../middlewares/userValidations");
 
 // Routes, separa cada rota 
-router.post("/register", validate, register);
+router.post("/register",userCreateValidation(), validate, register);
 
 //Exporta as rotas criadas: usa lá no centralizador das rotas 
 module.exports = router;
