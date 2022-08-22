ed060eb6aafaeee02a6221865cc552769b91122a
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-21 21:40:19 -0300

    Registro do usuário , controller

--- 12_REACTGRAM/backend/controllers/UserController.js
+++ 12_REACTGRAM/backend/controllers/UserController.js
@@ -18,9 +18,51 @@ const generateToken = (id) => {
 
 // Register user and sign in, registra e permite logar
 const register = async (req, res) => {
-    //resposta do chamado da rota:
-    res.send("Registro!");
+    //Pega os campos que chegam da requisição? 
+    const { name, email, password } = req.body;
+
+    //-----------------------------------------------------------------------------------------------------------
+    // check if user exists: não permite e-mail duplicado (findOne é um metodo do mongoose)
+    const user = await User.findOne({ email });
+    //valida se o usuario ja existe no sistema:
+    if (user) {
+        //caso true: gera uma erro re retorna.
+        res.status(422).json({ errors: ["Por favor, utilize outro e-mail."] });
+        return;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------
+    // Generate password hash: senha poluida com um salt, gera uma senha hash
+    const salt = await bcrypt.genSalt();
+    //gera uma senha hash aleatória:
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Create user com campos obrigatórios: usando senha hash (create é um metodo do mongoose)
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash,
+    });
+
+    //-----------------------------------------------------------------------------------------------------------
+    // If user was created sucessfully, return the token : user criado com sucesso , cria e gera o token
+    //Checa se deu algum problema na criação
+    if (!newUser) {
+        //Erro genérico: validar pelo logs
+        res.status(422).json({
+            errors: ["Houve um erro, por favor tente novamente mais tarde."],
+        });
+        return;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------
+    //Se der sucesso retorna 201 OK , com o ID e o Token
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
+    });
 };
+
 
 //Exporta as função:
 module.exports = {
