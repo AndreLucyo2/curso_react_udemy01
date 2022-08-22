c5952d6b0b7606cb15e2af4fbca2e66efbf1802a
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-21 23:54:00 -0300

    insertPhoto

--- 12_REACTGRAM/backend/controllers/UserController.js
+++ 12_REACTGRAM/backend/controllers/UserController.js
@@ -1,3 +1,4 @@
+//Model 
 const User = require("../models/User");
 
 //bcryptjs
--- 12_REACTGRAM/backend/routes/Router.js
+++ 12_REACTGRAM/backend/routes/Router.js
@@ -5,6 +5,9 @@ const router = express();
 
 //importa a rota relacionada ao usuario:
 router.use("/api/users", require("./UserRoutes"));
+//importa das rotas de fotos
+router.use("/api/photos", require("./PhotoRoutes"));
+
 
 //teste de rotas express: fazer um GET na url http://localhost:5000/
 router.get("/", (req, res) => {
