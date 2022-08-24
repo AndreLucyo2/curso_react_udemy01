466d42f11ac2e426ae751e30ae7f579154c95d55
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-23 22:35:14 -0300

    Iniciado criação da navbar e footer

--- 12_REACTGRAM/frontend/src/App.js
+++ 12_REACTGRAM/frontend/src/App.js
@@ -7,7 +7,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // components
-
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 // pages
 import Home from "./pages/Home/Home";
@@ -15,15 +16,18 @@ import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 
 
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
