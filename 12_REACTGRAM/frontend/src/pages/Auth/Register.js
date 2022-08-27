f9d097927c45f933b2b7d3de0eb148cc77d98db6
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-27 13:50:05 -0300

    Criando states do form de cadastro

--- 12_REACTGRAM/frontend/src/pages/Auth/Register.js
+++ 12_REACTGRAM/frontend/src/pages/Auth/Register.js
@@ -9,9 +9,23 @@ import { useState, useEffect } from "react";
 // Redux
 
 const Register = () => {
+    //states:
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        const user = {
+            name,
+            email,
+            password,
+            confirmPassword,
+        };
+
+        console.log(user);
 
     };
 
@@ -21,12 +35,31 @@ const Register = () => {
             <h2>ReactGram</h2>
             <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Nome" />
-                <input type="text" placeholder="E-mail" />
-                <input type="text" placeholder="Senha" />
-                <input type="text" placeholder="Confirme a senha" />
+                <input
+                    type="text"
+                    placeholder="Nome"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name || ''}
+                />
+                <input
+                    type="email"
+                    placeholder="E-mail"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email || ''}
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password || ''}
+                />
+                <input
+                    type="password"
+                    placeholder="Confirme a senha"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    value={confirmPassword || ''}
+                />
                 <input type="submit" value="Cadastrar" />
-
             </form>
             <p>
                 Já tem conta? <Link to="/login">Clique aqui</Link>
