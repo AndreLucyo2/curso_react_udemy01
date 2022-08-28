edf13895000e924a2c787a40dac1585968848821
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-28 15:02:41 -0300

    3 - completar o Slice de perfil de usuário

--- 12_REACTGRAM/frontend/src/slices/userSlice.js
+++ 12_REACTGRAM/frontend/src/slices/userSlice.js
@@ -11,8 +11,22 @@ const initialState = {
 };
 
 //funções
+// Get user details, for edit data, pega os dados para por na tela
+export const profile = createAsyncThunk(
+    "user/profile",
+    async (user, thunkAPI) => {
+        //obtem o token la do redux, pega do authSlice
+        const token = thunkAPI.getState().auth.user.token;
+        //dispara o get para a API
+        const data = await userService.profile(user, token);
 
-//reducer 
+        console.log(data);
+
+        return data;
+    }
+);
+
+//reducer , com o construtor do reducer pelos estados
 export const userSlice = createSlice({
     name: "user",
     initialState,
@@ -21,7 +35,23 @@ export const userSlice = createSlice({
             state.message = null;
         },
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(profile.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(profile.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.user = action.payload;
+            })
+    }
 });
+
+
+
 
 //Exportar as funções:
 export const { resetMessage } = userSlice.actions;
