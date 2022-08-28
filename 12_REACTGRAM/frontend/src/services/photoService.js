6be942b29b28610cb76833da91bdc0e930232469
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-28 20:06:35 -0300

    Service para inserir fotos

--- 12_REACTGRAM/frontend/src/services/photoService.js
+++ 12_REACTGRAM/frontend/src/services/photoService.js
@@ -1,5 +1,25 @@
 import { api, requestConfig } from "../utils/config";
 
-const photoService = {};
+// Publish an user's photo, manda tru no ultimo argumento pois tem foto envolvida
+const publishPhoto = async (data, token) => {
+    //monta a request
+    const config = requestConfig("POST", data, token, true);
+
+    try {
+        //manda a requisição para a api
+        const res = await fetch(api + "/photos", config)
+            .then((res) => res.json())
+            .catch((err) => err);
+
+        return res;
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const photoService = {
+    publishPhoto,
+};
 
 export default photoService;
