206742e4024082de3a0975b6a304a5a231cb7f0b
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-28 20:33:26 -0300

    Service de resgatar fotos do usuário

--- 12_REACTGRAM/frontend/src/services/photoService.js
+++ 12_REACTGRAM/frontend/src/services/photoService.js
@@ -18,8 +18,29 @@ const publishPhoto = async (data, token) => {
     }
 };
 
+// Get user photos
+const getUserPhotos = async (id) => {
+    //monta a request:
+    const config = requestConfig("GET");
+
+    try {
+        //dispara a requisição do para a API com o ID
+        const res = await fetch(api + "/photos/user/" + id, config)
+            .then((res) => res.json())
+            .catch((err) => err);
+
+        return res;
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
 const photoService = {
     publishPhoto,
+    getUserPhotos,
+
 };
 
 export default photoService;
