09cd3133efa6d9a7011479dbffd07add4e0a5f2b
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-28 14:32:43 -0300

    Estruturando página de edição de perfil + css

---
+++ 12_REACTGRAM/frontend/src/pages/EditProfile/EditProfile.js
@@ -0,0 +1,87 @@
+import "./EditProfile.css";
+
+import { uploads } from "../../utils/config";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+import { profile, updateProfile, resetMessage } from "../../slices/userSlice";
+
+// Components
+import Message from "../../components/Message";
+
+
+const EditProfile = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [profileImage, setProfileImage] = useState("");
+    const [bio, setBio] = useState("");
+    const [previewImage, setPreviewImage] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+
+
+    return (
+        <div id="edit-profile">
+            <h2>Edite seus dados</h2>
+            <p className="subtitle">
+                Adicione uma imagem de perfil, e conte mais um pouco sobre você...
+            </p>
+
+            {/* preview da imagem */}
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nome"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name || ""}
+                />
+
+                {/* Não é permitido alterar o e-mail, campo fica desabilitado */}
+                <input
+                    type="email"
+                    placeholder="E-mail"
+                    disabled
+                    value={email || ""}
+                />
+
+                <label>
+                    <span>Imagem de Perfil:</span>
+                    <input type="file" onChange={handleFile} />
+                </label>
+
+                <label>
+                    <span>Bio:</span>
+                    <input
+                        type="text"
+                        placeholder="Descrição do perfil"
+                        onChange={(e) => setBio(e.target.value)}
+                        value={bio || ""}
+                    />
+                </label>
+
+                <label>
+                    <span>Quer alterar sua senha?</span>
+                    <input
+                        type="password"
+                        placeholder="Digite sua nova senha..."
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password || ""}
+                    />
+                </label>
+
+                <input type="submit" value="Atualizar" />
+            </form>
+
+        </div>
+    )
+}
+
+export default EditProfile
