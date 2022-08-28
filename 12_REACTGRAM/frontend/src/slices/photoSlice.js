39a75573f38686c6439137cd19497afde40a466a
AndreLucyo <andre.lucio@nextar.com.br>
2022-08-28 20:12:36 -0300

    Slice de inserção de foto

--- 12_REACTGRAM/frontend/src/slices/photoSlice.js
+++ 12_REACTGRAM/frontend/src/slices/photoSlice.js
@@ -12,6 +12,26 @@ const initialState = {
 };
 
 //Funções
+//--- PUBLICAR FOTO --------------------------------------------------------------------------
+// Publish an user's photo
+export const publishPhoto = createAsyncThunk("photo/publish",
+    async (photo, thunkAPI) => {
+        //obtem o token
+        const token = thunkAPI.getState().auth.user.token;
+
+        //pega os dados da requisição
+        const data = await photoService.publishPhoto(photo, token);
+
+        console.log(data.errors);
+
+        // Check for errors, retorna o erro para o form
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0]);
+        }
+
+        return data;
+    }
+);
 
 
 export const photoSlice = createSlice({
@@ -22,6 +42,26 @@ export const photoSlice = createSlice({
             state.message = null;
         },
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(publishPhoto.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(publishPhoto.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.photo = action.payload;//manipula arraim adicionanro no primeiro item do array
+                state.photos.unshift(state.photo);
+                state.message = "Foto publicada com sucesso!";
+            })
+            .addCase(publishPhoto.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+                state.photo = {};
+            })
+    }
 });
 
 
