d134432ed6f737c06dfe0ec72379a040a0cd55a5
AndreLucyo <andre.lucio@nextar.com.br>
2022-09-11 14:28:17 -0300

    feat(foto): Finalizando funcionalidade de comentário

--- 12_REACTGRAM/frontend/src/pages/Photo/Photo.js
+++ 12_REACTGRAM/frontend/src/pages/Photo/Photo.js
@@ -15,7 +15,7 @@ import { useParams } from "react-router-dom";
 import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 // Redux
-import { getPhoto, like } from "../../slices/photoSlice";
+import { getPhoto, like, comment } from "../../slices/photoSlice";
 import LikeContainer from "../../components/LikeContainer";
 
 function Photo() {
@@ -32,22 +32,41 @@ function Photo() {
     //pega alguns dados da foto
     const { photo, loading, error, message } = useSelector((state) => state.photo);
 
-    //comentario
+    //Manipular comentário
+    const [commentText, setCommentText] = useState();
 
     //Load foto data, entra na pagina ja vai ter a foto sendo carregada
     useEffect(() => {
         dispatch(getPhoto(id));
     }, [dispatch, id]);
 
-
-    //Like a photo e comentario
+    //Insert a Like a photo e comentario
     const handleLike = () => {
         //executa o like na foto
         dispatch(like(photo._id));
-        
+
         //reseta a menssagem
         resetMessage();
     };
+
+    // Insert a comment
+    const handleComment = (e) => {
+        e.preventDefault();
+
+        //monta os dados do comentário
+        const commentData = {
+            comment: commentText,
+            id: photo._id,
+        };
+
+        dispatch(comment(commentData));
+        
+        //reseta o form, limpa o campo
+        setCommentText("");
+
+        resetMessage();
+    };
+
 
     //Caso em load
     if (loading) {
@@ -63,8 +82,42 @@ function Photo() {
                 {error && <Message msg={error} type="error" />}
                 {message && <Message msg={message} type="success" />}
             </div>
+            <div className="comments">
+                {/* Testa se a entidade existe */}
+                {photo.comments && (
+                    <>
+                        <h3>Comentários ({photo.comments.length}):</h3>
+                        <form onSubmit={handleComment}>
+                            <input
+                                type="text"
+                                placeholder="Insira seu comentário..."
+                                onChange={(e) => setCommentText(e.target.value)}
+                                value={commentText || ""}
+                            />
+                            <input type="submit" value="Enviar" />
+                        </form>
+                        {photo.comments.length === 0 && <p>Não há comentários...</p>}
+                        {photo.comments.map((comment) => (
+                            <div className="comment" key={comment.comment}>
+                                <div className="author">
+                                    {comment.userImage && (
+                                        <img
+                                            src={`${uploads}/users/${comment.userImage}`}
+                                            alt={comment.userName}
+                                        />
+                                    )}
+                                    <Link to={`/users/${comment.userId}`}>
+                                        <p>{comment.userName}</p>
+                                    </Link>
+                                </div>
+                                <p>{comment.comment}</p>
+                            </div>
+                        ))}
+                    </>
+                )}
+            </div>
         </div>
     );
-}
+};
 
 export default Photo
